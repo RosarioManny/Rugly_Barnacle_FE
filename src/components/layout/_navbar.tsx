561fbe97c2d036324c129078ed688b0dc1b10994
RosarioManny <rosario.emm47@gmail.com>
2025-07-02 18:36:44 -0400

    Nav Dropdown

--- src/components/layout/_navbar.tsx
+++ src/components/layout/_navbar.tsx
@@ -1,15 +1,40 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
+
 export const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
 
   return (
     <>
-      <Link to="/"> Home </Link>
-      <Link to="/about"> About </Link>
-      <Link to="/faq"> FAQ </Link>
-      <Link to="/shop"> Shop </Link>
-      <Link to="/cart"> Cart </Link>
-      <Link to="/portfolio"> Portfolio </Link>
-      <Link to="/checkout"> Checkout </Link>
+      {/* Nav Links */}
+      <section>
+        <Link to="/"> Home </Link>
+        <Link 
+        to="/about" 
+        onMouseEnter={() => {setIsOpen(!isOpen)}}
+        onMouseLeave={() => {setIsOpen(!isOpen)}}
+        > About 
+        {isOpen && 
+          <>
+            <Link to="/about">About</Link>
+            <Link to="/faq">Faq</Link>
+          </>
+        }
+        </Link>
+        <Link to="/shop"> Shop </Link>
+        <Link to="/portfolio"> Portfolio </Link>
+      </section>
+      {/* Home - Brand image small logo*/}
+      <section>
+        <Link to="/">
+          <img src="/assets/Logo/Rugly_Barnacle_192x192.png"/>
+        </Link>
+      </section>
+      {/* Cart Link */}
+      <section>
+        <Link to="/cart"> Cart </Link>
+      </section>
     </>
   )
 }
