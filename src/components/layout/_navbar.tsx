af2c1ef1ac4ef3ba358b0a8158bc319086c281aa
RosarioManny <rosario.emm47@gmail.com>
2025-07-03 16:55:42 -0400

    Nav Dropdown Animation

--- src/components/layout/_navbar.tsx
+++ src/components/layout/_navbar.tsx
@@ -4,31 +4,50 @@ import { Link } from "react-router-dom"
 export const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false)
 
+  const handleDropdown = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <nav className="">
       {/* Nav Links */}  
       <div className="navbar">
         <Link to="/portfolio">Portfolio</Link>
         {/* <Link to="/shop">Shop</Link> */}
+
         <div className="dropdown">
-          <button className="dropbtn">Shop 
-            <i className="fa fa-caret-down"></i>
+          <button 
+          className="dropbtn pointer"
+          onClick={handleDropdown}>
+            Shop 
+            <i className={`
+              ${isOpen ? "border-t-bittersweet rotate-x-180" : "border-t-majorelle"} 
+              transition-transform duration-600 border-t-10 border-solid ease-in-out
+              fa fa-caret-down`} 
+              />
           </button>
-          <div className="dropdown-content">
+
+          <div className={`
+            ${isOpen ? " max-h-96" : " max-h-0"}
+            overflow-hidden transition-all duration-500 ease-in-out
+            dropdown-content`}>
             <Link to="/about">About Me</Link>
             <Link to="/faq">Faq</Link>
             <Link to="/contact">Contact</Link>
           </div>
         </div> 
+
         <div className="dropdown">
-          <button className="dropbtn">About 
-            <i className="fa fa-caret-down"></i>
+          <button className="dropbtn">
+            About 
+
           </button>
+{/* 
           <div className="dropdown-content">
             <Link to="/about">About Me</Link>
             <Link to="/faq">Faq</Link>
             <Link to="/contact">Contact</Link>
-          </div>
+          </div> */}
         </div> 
       </div>
       {/* Home - Brand image small logo*/}
