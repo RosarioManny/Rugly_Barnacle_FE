12cf975b8123835d8d3b5df17043307066e8daa5
RosarioManny <rosario.emm47@gmail.com>
2025-07-17 12:59:54 -0400

    Populate About

--- src/components/layout/_footer.tsx
+++ src/components/layout/_footer.tsx
@@ -21,80 +21,84 @@ export const Footer = () => {
   
   const links: string[] = [ "shop",  "about", "portfolio", "faq"];
   return (
-    <footer className="bg-space_cadet w-full">
-      <CtaWavesBg className="bg-mauve fill-space_cadet h-full -b-[10px]"/>
-      <div className="w-full flex justify-end">
-        <img className=" size-16" src="assets/design/icons/X_Star_Pink.webp" aria-hidden="true" alt="X-Star Design Marker" />
+    <>
+      <div className="relative">
+        <CtaWavesBg className="absolute  w-full h-auto fill-space_cadet -translate-y-full"/>
       </div>
-      <main aria-label="Footer Content" className="text-center flex flex-col items-center">
-      {/* NEWSLETTER */}
-        <NewsLetterForm/>
-        {/* SOCIALS */}
-        <section className="my-4">
-          <h2 className="
-          heading_text
-          text-mauve 
-          underline underline-offset-6 "> 
-            Follow Me!
-          </h2>
-          <ul className="flex space-x-6 m-2">
-            {socialMedia.map(({ Social }, idx) => (
-              <li key={`${Social}-${idx} group-text-fleece`}>
-                <Social  
-                className="
-                pointer duration-300  ease-in-out
-                size-10 text-fleece hover:text-breeze hover:scale-105 
-                active:text-breeze active:scale-105 
-                focus:text-breeze focus:scale-105"
-                />
-              </li>
-            ))}
-          </ul>
-        </section>
-        {/* CONTACT ME */}
-        <section className="my-4 space-y-4 text-fleece flex flex-col items-center">
-          <h2 className="
-          heading_text 
-          text-mauve
-          underline underline-offset-6 "> 
-            Contact Me!
-          </h2>
-          <p> 718-111-2223</p>
-          <p> myemail@anaddress.com</p>
-        </section>
-        {/* QUICK LINKS */}
-        <section className="my-4 flex flex-col items-center">
-          <h2 className="
+      <footer className="bg-space_cadet w-full relative">
+        <div className="w-full flex justify-end">
+          <img className=" size-16" src="assets/design/icons/X_Star_Pink.webp" aria-hidden="true" alt="X-Star Design Marker" />
+        </div>
+        <main aria-label="Footer Content" className="text-center flex flex-col items-center">
+        {/* NEWSLETTER */}
+          <NewsLetterForm/>
+          {/* SOCIALS */}
+          <section className="my-4">
+            <h2 className="
+            heading_text
+            text-mauve 
+            underline underline-offset-6 "> 
+              Follow Me!
+            </h2>
+            <ul className="flex space-x-6 m-2">
+              {socialMedia.map(({ Social }, idx) => (
+                <li key={`${Social}-${idx} group-text-fleece`}>
+                  <Social  
+                  className="
+                  pointer duration-300  ease-in-out
+                  size-10 text-fleece hover:text-breeze hover:scale-105 
+                  active:text-breeze active:scale-105 
+                  focus:text-breeze focus:scale-105"
+                  />
+                </li>
+              ))}
+            </ul>
+          </section>
+          {/* CONTACT ME */}
+          <section className="my-4 space-y-4 text-fleece flex flex-col items-center">
+            <h2 className="
             heading_text 
             text-mauve
-            underline underline-offset-6 ">
-            Quick link
-          </h2>
-          <ul className="text-fleece flex space-x-4 m-4">
-          {links.map((link, idx) => (
-            <li key={`${link}-${idx}`}>
-              <Link 
-                to={`/${link}`} 
-                className="
-                  pointer duration-300  ease-in-out
-                  size-10 text-fleece 
-                  hover:text-breeze 
-                  active:text-breeze  
-                  focus:text-breeze">
-                {link.charAt(0).toUpperCase() + link.slice(1)}
-              </Link>
-            </li>
-          ))}
-          </ul>
-        </section>
-        <section className="flex justify-start w-full">
-        <img 
-          className="m-10 h-10 w-10" 
-          src="/assets/design/icons/X_Star_Teal-Blue.webp" 
-          aria-hidden="true"
-          alt="Teal Star decorator" />
-        </section>
-      </main>
-    </footer>
+            underline underline-offset-6 "> 
+              Contact Me!
+            </h2>
+            <p> 718-111-2223</p>
+            <p> myemail@anaddress.com</p>
+          </section>
+          {/* QUICK LINKS */}
+          <section className="my-4 flex flex-col items-center">
+            <h2 className="
+              heading_text 
+              text-mauve
+              underline underline-offset-6 ">
+              Quick link
+            </h2>
+            <ul className="text-fleece flex space-x-4 m-4">
+            {links.map((link, idx) => (
+              <li key={`${link}-${idx}`}>
+                <Link 
+                  to={`/${link}`} 
+                  className="
+                    pointer duration-300  ease-in-out
+                    size-10 text-fleece 
+                    hover:text-breeze 
+                    active:text-breeze  
+                    focus:text-breeze">
+                  {link.charAt(0).toUpperCase() + link.slice(1)}
+                </Link>
+              </li>
+            ))}
+            </ul>
+          </section>
+          <section className="flex justify-start w-full">
+          <img 
+            className="m-10 h-10 w-10" 
+            src="/assets/design/icons/X_Star_Teal-Blue.webp" 
+            aria-hidden="true"
+            alt="Teal Star decorator" />
+          </section>
+        </main>
+      </footer>
+    </>
   )
 }
--- src/pages/About/about.tsx
+++ src/pages/About/about.tsx
@@ -1,10 +1,44 @@
+import { Header } from "../../components/layout/_header"
+import { ShopBtn } from "../../components/ui/buttons/btn_shop"
+import { Shop } from "../Shop/shop"
+
 export const About = () => {
 
   return (
-    <>
-      <h1>
-        I am the About page 👤
-      </h1>
-    </>
+    <main aria-label="About Me Page">
+      <Header
+        title="About Me"
+        tagline="Learn what Rugly Barnacle is about!"
+        img="/assets/design/logo/RuglyBarnacle_Logo.webp"
+        img_alt="Rugly Barnacle Logo"/>
+        <section>
+          <h1>What is Rugly Barnacle</h1>
+          <img 
+          className="flex justify-start h-10 w-10" 
+          src="/assets/design/icons/Cross_Star_White.webp" 
+          aria-hidden="true" 
+          alt="Cross Star Design Marker" />
+        </section>
+        <section>
+          <p>
+            Hi! I'm Emilie, the tuft broad behind the Rugly Barnacle!
+            Tufting rugs started as a COVID hobby and quickly became my absolute passion and business. 
+            Every Rugly Barnacle piece is a celebration of the fandom community — because it's for the fan, by the fan.
+            So, if you're looking for a custom rug that's made just for you, look no further! 
+            Let me bring your favorite character, symbol, logo, art piece or any design of your wildest dream to life in rug form! 
+            Each rug is one of a kind and 100% handmade to exceed every customer's expectation. Ready?
+          </p>
+        </section>
+        <section className="py-16">
+          <p>Love what we’re about? Checkout our rug to see which one you like!</p>
+          <img 
+          className="flex justify-start h-10 w-10" 
+          src="/assets/design/icons/Cross_Star_White.webp" 
+          aria-hidden="true" 
+          alt="Cross Star Design Marker" />
+          <ShopBtn/>
+          <img src="/assets/design/logo/Banner_Blue_Logo_Phrase.png" alt="" />
+        </section>
+    </main>
   )
 }
